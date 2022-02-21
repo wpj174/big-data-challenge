c564cbad53c0bc6bbbef37bc57213219a42c52af
Bill Johnson <wpj174@gmail.com>
2022-02-20 20:01:22 -0500

    SQL Analysis Framework

---
+++ level-2/vine_analysis.sql
@@ -0,0 +1,21 @@
+-- Analysis of Vine Reviews
+
+-- Filter by Votes
+--    Filter for > 20 votes
+--    Filter for > 50% helpful vote percentage
+
+-- Describe Stats
+--    Paid reviews
+--    Unpaid reviews
+
+-- Determine the Percentage of Five-star Reviews Among Vine Reviews
+--    Number of paid reviews
+--    Number of paid 5-star reviews
+--    Pct of paid reviews that are 5-star
+
+-- Determine the Percentage of Five-star Reviews Among non-Vine Reviews
+--    Number of unpaid review
+--    Number of unpaid 5-start reviews
+--    Pct of unpaid reviews that are 5-star
+
+-- Submit a Summary of Findings and Analysis
